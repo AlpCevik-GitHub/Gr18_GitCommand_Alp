faa26faeef17419764a0b00b573c39bbbd786174
Alp <calptekincevik@gmail.com>
2023-03-31 22:52:23 +0300

    my second commit

--- src/test/java/MyClass_Alp.java
+++ src/test/java/MyClass_Alp.java
@@ -3,5 +3,6 @@ public class MyClass_Alp {
 
     public static void main(String[] args) {
         System.out.println("First commit on my local");
+        System.out.println("Second commit on my local");
     }
 }
